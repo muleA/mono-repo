d9d4b4a51fa926e8dccfd78b593e68fd1fd25438
mulugeta <mulugeta.adamu97@gmail.com>
2022-09-05 16:51:05 +0300

    models and features added

---
+++ apps/back-office/online-suke/models/product.ts
@@ -0,0 +1,13 @@
+export default interface Product{
+  id:string;
+  name:string;
+  category:string;
+  description:string;
+  image:string;
+  price:number;
+  brand:string;
+  rating:string;
+  numberOfReviews:number;
+  countInStock:number;
+  slug:string;
+}
---
+++ apps/back-office/online-suke/models/user.ts
@@ -0,0 +1,8 @@
+export interface User{
+  id:string;
+  name:string;
+  image:string;
+  email:string;
+  password:string;
+  isAdmin:boolean
+}
