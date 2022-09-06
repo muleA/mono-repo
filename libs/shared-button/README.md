810d2400f45ee4d5aa428531e1ecaa90209dd0f3
mulugeta <mulugeta.adamu97@gmail.com>
2022-09-06 15:30:06 +0300

    storybook added

---
+++ libs/shared-button/README.md
@@ -0,0 +1,7 @@
+# shared-button
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test shared-button` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/shared-button/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'shared-button',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/shared-button',
+};
